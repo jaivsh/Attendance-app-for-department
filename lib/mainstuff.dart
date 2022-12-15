115dbf062f37341d4e4c0d5215b7f26654696a07
Jaivardhan Shukla <aarshorai22@gmail.com>
2022-12-15 16:33:01 +0530

    Added more

--- lib/mainstuff.dart
+++ lib/mainstuff.dart
@@ -24,11 +24,83 @@ class _MainScreenState extends State<MainScreen1> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text(
-              'Professor: ',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Professor:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  ' Dr. Ritesh Lokhande',
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
             ),
+            Container(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Total Classes Attended:',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    Text(
+                      ' 12/15',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                )),
+            Container(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Attendance Percentage:',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    Text(
+                      ' 80.0 %',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                )),
+            Flexible(
+                child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(columns: const [
+                      DataColumn(
+                        label: Text('Serial'),
+                      ),
+                      DataColumn(
+                        label: Text('Date'),
+                      ),
+                      DataColumn(
+                        label: Text('Presentees'),
+                      ),
+                      DataColumn(
+                        label: Text('Absentees'),
+                      ),
+                      DataColumn(
+                        label: Text('Time of Attendance'),
+                      ),
+                    ], rows: const [
+                      DataRow(cells: [
+                        DataCell(Text('1')),
+                        DataCell(Text('2/1/23')),
+                        DataCell(Text('P')),
+                        DataCell(Text('A')),
+                        DataCell(Text('12:29:12 PM'))
+                      ])
+                    ])))
           ],
         ),
       ),
